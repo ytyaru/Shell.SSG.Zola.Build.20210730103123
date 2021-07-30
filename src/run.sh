0fc529177823a32370d2947ff000dfaa59bca118
ytyaru <yttry0@gmail.com>
2021-07-30 10:40:38 +0900

    add install

--- src/run.sh
+++ src/run.sh
@@ -7,11 +7,21 @@ set -Ceu
 Run() {
 	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
 	cd "$HERE"
-	Dounload() { wget https://github.com/getzola/zola/archive/refs/tags/v0.14.0.tar.gz; }
-	Expand() { tar xf v0.14.0.tar.gz; }
-	Build() { cd zola-0.14.0; cargo build --features search/indexing-ja; }
+	VERSION=0.14.0
+	ARC_NAME=v$VERSION.tar.gz
+	DIR_NAME=zola-$VERSION
+	PATH_DIR=/home/pi/root/sys/env/tool
+	TO=$PATH_DIR/ssg
+	Dounload() { wget https://github.com/getzola/zola/archive/refs/tags/$ARC_NAME; }
+	Expand() { tar xf $ARC_NAME; }
+	Build() { cd $DIR_NAME; cargo build --features search/indexing-ja; }
+	Copy() { cd ..; cp ./zola-0.14.0 "$TO"; }
+	Link() { ln -s $TO/$DIR_NAME/target/debug/zola $PATH_DIR/zola; }
 	Dounload
 	Expand
 	Build
+	Copy
+	Link
+	zola --version
 }
 Run "$@"
